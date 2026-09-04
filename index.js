972892919460d49c8fd11a70ea41e9f8618a0b3f
Iwuogu Steven <dovadallas661@gmail.com>
2026-09-04 05:12:13 +0100

    Prepare backend for deployment

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import taskRouter from './routes/taskRoutes.js'
 import userRouter from './routes/userRoutes.js'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
+
 app.use('/api/v1/tasks', taskRouter)
 app.use('/api/v1/users', userRouter)
-
 
 app.listen(PORT, () => {
     console.log(`App is running on port: ${PORT}`)
